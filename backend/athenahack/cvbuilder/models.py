89e3bfa832df16512e5e8d69f923ea4628fd3f05
jessiebelle <jessieauguste@gmail.com>
2022-06-11 19:53:17 +0100

    migration run

--- backend/athenahack/cvbuilder/models.py
+++ backend/athenahack/cvbuilder/models.py
@@ -7,13 +7,12 @@ class Language(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+
 class User(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
-    company = models.ForeignKey
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-
-class Company(models.Model):
-    id = models.BigAutoField()
