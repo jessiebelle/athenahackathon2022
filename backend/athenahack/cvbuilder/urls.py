abacacb14afa971d58fc70bb624d6b33ff280d27
jessiebelle <jessieauguste@gmail.com>
2022-06-11 21:38:14 +0100

    added additional views

--- backend/athenahack/cvbuilder/urls.py
+++ backend/athenahack/cvbuilder/urls.py
@@ -1,7 +1,17 @@
+from django.db import router
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from . import views
+from .views import CVViewSet, CVFinishedViewSet, CVQuestionsViewSet
 
-urlpatterns = [
-    path('', views.index, name='index'),
-]
+app_name = "insights"
+
+router = DefaultRouter()
+router.register(r"CV/", CVFinishedViewSet, basename="r1")
+router.register(
+    r"CV-questions/", CVQuestionsViewSet, basename="questions"
+),
+router.register(r"cv/create/", CVViewSet, basename="create-r1"),
+
+urlpatterns = router.urls
--- backend/athenahack/cvbuilder/views.py
+++ backend/athenahack/cvbuilder/views.py
@@ -1,17 +1,46 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django_filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from athenahackathon2022.backend.athenahack.cvbuilder.models import CV
+from athenahackathon2022.backend.athenahack.cvbuilder.serializers import CVSerializers
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-class CVViewSetR1(ReadOnlyModelViewSet):
+class CVViewSet(ReadOnlyModelViewSet):
     queryset = CV.objects.all()
-    serializer_class = InsightSerializers.InsightQuestionSerializer
+    serializer_class = CVSerializers.CVResponseSerializer
+    filter_backends = (OrderingFilter, DjangoFilterBackend)
+    ordering = ("insight_question",)
+
+    def get_queryset(self):
+        return CV.objects.filter_by_user(user=self.request.user)
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        return response
+
+class CVFinishedViewSet(ReadOnlyModelViewSet):
+    queryset = CV.objects.all()
+    serializer_class = CVSerializers.CVResponseSerializer
+    filter_backends = (OrderingFilter, DjangoFilterBackend)
+    ordering = ("insight_question",)
+
+    def get_queryset(self):
+        return CV.objects.filter_by_user(user=self.request.user)
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        return response
+
+class CVQuestionsViewSet(ReadOnlyModelViewSet):
+    queryset = CV.objects.all()
+    serializer_class = CVSerializers.CVResponseSerializer
     filter_backends = (OrderingFilter, DjangoFilterBackend)
     ordering = ("insight_question",)
 
