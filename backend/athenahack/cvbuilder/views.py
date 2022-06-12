dcc42f67b0895598ae55786a22ff5cb4dc0332bc
jessiebelle <jessieauguste@gmail.com>
2022-06-12 11:04:54 +0100

    new view

--- backend/athenahack/cvbuilder/views.py
+++ backend/athenahack/cvbuilder/views.py
@@ -2,10 +2,13 @@ from django.http import HttpResponse
 from django_filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from .models import CV
+from .models import CV, Experience, Skill, AboutMe, User
 from .serializers import CVSerializers
 
 
@@ -24,6 +27,19 @@ class CVViewSet(ReadOnlyModelViewSet):
         return response
 
 
+class ListCV(APIView):
+
+    def get(self, request):
+        user = request.user
+        user_details = User.objects.filter(user=user)
+        about_me = AboutMe.objects.filer(user=user)
+        skill = Skill.objects.filter(user=user)
+        experience = Experience.objects.filter(user=user)
+
+        serializer = CVSerializers.CVResponseSerializer(user_details, about_me, skill, experience, many=True)
+        return Response(serializer.data)
+
+
 class CVFinishedViewSet(ReadOnlyModelViewSet):
     queryset = CV.objects.all()
     serializer_class = CVSerializers.CVResponseSerializer
@@ -36,6 +52,17 @@ class CVFinishedViewSet(ReadOnlyModelViewSet):
     def list(self, request, *args, **kwargs):
         response = super().list(request, *args, **kwargs)
         return response
+
+    @action(methods=["get"], detail=True, url_path="cv", url_name="cv")
+    def publish(self, request, *args, **kwargs):
+        self.serializer_class = AssistPublishSerializerR1
+        instance = self.get_object()
+
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
 
 
 class CVQuestionsViewSet(ReadOnlyModelViewSet):
