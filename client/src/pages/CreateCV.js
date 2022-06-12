1a7db8ca2f6c3479d99940f564138dad368a4e1b
michaela <elagasteratou@gmail.com>
2022-06-12 10:05:18 +0100

    FE initial commit

---
+++ client/src/pages/CreateCV.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CreateCV() {
+  return (
+    <>
+      <div>CreateCV</div>
+    </>
+  );
+}
+
+export default CreateCV;
---
+++ client/src/pages/PreviewCV.js
@@ -0,0 +1,26 @@
+import { Stack } from '@mui/material';
+import React from 'react';
+import AboutMe from '../components/AboutMe';
+
+import Experience from '../components/Experience';
+import PrimaryButton from '../components/PrimaryButton';
+import SecondaryButton from '../components/SecondaryButton';
+import Skills from '../components/Skills';
+import DATA from '../userData';
+
+function PreviewCV() {
+  return (
+    <Stack>
+      <AboutMe aboutMe={DATA.about_me.about_me_text} />
+
+      <h1>Work Experience</h1>
+      <Experience experience={DATA.work_experience} />
+      <h1>Skills</h1>
+      <Skills skills={DATA.skills} />
+
+      <SecondaryButton text={'Submit your CV'} />
+    </Stack>
+  );
+}
+
+export default PreviewCV;
